d38a78d994727ea4dbdc051f7d76a817b94dc548
Fabian Herb <fabian@herb-clan.de>
2025-11-12 23:35:14 +0100

    Path vars

--- ardour.cmake
+++ ardour.cmake
@@ -1,4 +1,5 @@
-set(A ${CMAKE_CURRENT_SOURCE_DIR}/ardour)
+set(L ${CMAKE_CURRENT_SOURCE_DIR}/ardour/libs)
+set(A ${L}/ardour)
 
 find_package(Boost REQUIRED)
 find_package(LibXml2 REQUIRED)
@@ -16,85 +17,87 @@ find_path(LILV_INCLUDE_DIR
 add_custom_command(
     OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/pbd/signals_generated.h
     COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/pbd/
-    COMMAND python3 ${A}/libs/pbd/pbd/signals.py ${CMAKE_CURRENT_BINARY_DIR}/pbd/signals_generated.h
+    COMMAND python3 ${L}/pbd/pbd/signals.py ${CMAKE_CURRENT_BINARY_DIR}/pbd/signals_generated.h
 )
 
 add_library(ardourlib
-#    ${A}/libs/ardour/aarch64_neon_functions.cc
-    ${A}/libs/ardour/amp.cc
-#    ${A}/libs/ardour/analyser.cc
-#    ${A}/libs/ardour/analysis_graph.cc
-    ${A}/libs/ardour/arm_neon_functions.cc
-    ${A}/libs/ardour/async_midi_port.cc
-    ${A}/libs/ardour/audioanalyser.cc
-    ${A}/libs/ardour/audio_backend.cc
-    ${A}/libs/ardour/audio_buffer.cc
-    ${A}/libs/ardour/audioengine.cc
+#    ${A}/aarch64_neon_functions.cc
+    ${A}/amp.cc
+#    ${A}/analyser.cc
+#    ${A}/analysis_graph.cc
+    ${A}/arm_neon_functions.cc
+    ${A}/async_midi_port.cc
+    ${A}/audioanalyser.cc
+    ${A}/audio_backend.cc
+    ${A}/audio_buffer.cc
+    ${A}/audioengine.cc
 
-    ${A}/libs/ardour/globals.cc
-    ${A}/libs/ardour/graph.cc
-    ${A}/libs/ardour/graph_edges.cc
-    ${A}/libs/ardour/graphnode.cc
-    ${A}/libs/ardour/iec1ppmdsp.cc
-    ${A}/libs/ardour/iec2ppmdsp.cc
-    ${A}/libs/ardour/import.cc
-    ${A}/libs/ardour/import_pt.cc
-    ${A}/libs/ardour/instrument_info.cc
-    ${A}/libs/ardour/internal_return.cc
-    ${A}/libs/ardour/internal_send.cc
-    ${A}/libs/ardour/io.cc
-    ${A}/libs/ardour/io_plug.cc
-    ${A}/libs/ardour/io_processor.cc
-    ${A}/libs/ardour/io_tasklist.cc
-    ${A}/libs/ardour/kmeterdsp.cc
-    ${A}/libs/ardour/ladspa_plugin.cc
-    ${A}/libs/ardour/latent.cc
-    ${A}/libs/ardour/legatize.cc
-    ${A}/libs/ardour/library.cc
-    ${A}/libs/ardour/linux_vst_support.cc
-    ${A}/libs/ardour/load-save-session-collection.sh
-    ${A}/libs/ardour/load-save-session.sh
-    ${A}/libs/ardour/location.cc
-    ${A}/libs/ardour/location_importer.cc
-    ${A}/libs/ardour/ltc_file_reader.cc
-    ${A}/libs/ardour/ltc_slave.cc
-#    ${A}/libs/ardour/lua_api.cc
-#    ${A}/libs/ardour/luabindings.cc
-#    ${A}/libs/ardour/luaproc.cc
-#    ${A}/libs/ardour/luascripting.cc
-    ${A}/libs/ardour/lufs_meter.cc
-    ${A}/libs/ardour/lv2_evbuf.cc
-    ${A}/libs/ardour/lv2_plugin.cc
-    ${A}/libs/ardour/lxvst_plugin.cc
-#    ${A}/libs/ardour/mac_vst_plugin.cc
-#    ${A}/libs/ardour/mac_vst_support.cc
-    ${A}/libs/ardour/meter.cc
-    ${A}/libs/ardour/midi_automation_list_binder.cc
-    ${A}/libs/ardour/midi_buffer.cc
-    ${A}/libs/ardour/midi_channel_filter.cc
-    ${A}/libs/ardour/midi_clock_slave.cc
-    ${A}/libs/ardour/mididm.cc
-    ${A}/libs/ardour/midi_model.cc
-    ${A}/libs/ardour/midi_patch_manager.cc
-    ${A}/libs/ardour/midi_playlist.cc
-    ${A}/libs/ardour/midi_port.cc
-    ${A}/libs/ardour/midiport_manager.cc
-    ${A}/libs/ardour/midi_region.cc
-    ${A}/libs/ardour/midi_ring_buffer.cc
-    ${A}/libs/ardour/midi_scene_change.cc
-    ${A}/libs/ardour/midi_scene_changer.cc
-    ${A}/libs/ardour/midi_source.cc
-    ${A}/libs/ardour/midi_state_tracker.cc
-    ${A}/libs/ardour/midi_stretch.cc
-    ${A}/libs/ardour/midi_track.cc
-    ${A}/libs/ardour/midi_ui.cc
-    ${A}/libs/ardour/minibpm.cc
-    ${A}/libs/ardour/mix.cc
+    ${A}/globals.cc
+    ${A}/graph.cc
+    ${A}/graph_edges.cc
+    ${A}/graphnode.cc
+    ${A}/iec1ppmdsp.cc
+    ${A}/iec2ppmdsp.cc
+    ${A}/import.cc
+    ${A}/import_pt.cc
+    ${A}/instrument_info.cc
+    ${A}/internal_return.cc
+    ${A}/internal_send.cc
+    ${A}/io.cc
+    ${A}/io_plug.cc
+    ${A}/io_processor.cc
+    ${A}/io_tasklist.cc
+    ${A}/kmeterdsp.cc
+    ${A}/ladspa_plugin.cc
+    ${A}/latent.cc
+    ${A}/legatize.cc
+    ${A}/library.cc
+    ${A}/linux_vst_support.cc
+    ${A}/load-save-session-collection.sh
+    ${A}/load-save-session.sh
+    ${A}/location.cc
+    ${A}/location_importer.cc
+    ${A}/ltc_file_reader.cc
+    ${A}/ltc_slave.cc
+#    ${A}/lua_api.cc
+#    ${A}/luabindings.cc
+#    ${A}/luaproc.cc
+#    ${A}/luascripting.cc
+    ${A}/lufs_meter.cc
+    ${A}/lv2_evbuf.cc
+    ${A}/lv2_plugin.cc
+    ${A}/lxvst_plugin.cc
+#    ${A}/mac_vst_plugin.cc
+#    ${A}/mac_vst_support.cc
+    ${A}/meter.cc
+    ${A}/midi_automation_list_binder.cc
+    ${A}/midi_buffer.cc
+    ${A}/midi_channel_filter.cc
+    ${A}/midi_clock_slave.cc
+    ${A}/mididm.cc
+    ${A}/midi_model.cc
+    ${A}/midi_patch_manager.cc
+    ${A}/midi_playlist.cc
+    ${A}/midi_port.cc
+    ${A}/midiport_manager.cc
+    ${A}/midi_region.cc
+    ${A}/midi_ring_buffer.cc
+    ${A}/midi_scene_change.cc
+    ${A}/midi_scene_changer.cc
+    ${A}/midi_source.cc
+    ${A}/midi_state_tracker.cc
+    ${A}/midi_stretch.cc
+    ${A}/midi_track.cc
+    ${A}/midi_ui.cc
+    ${A}/minibpm.cc
+    ${A}/mix.cc
 
-    ${A}/libs/ardour/send.cc
-    ${A}/libs/ardour/session_bundles.cc
-    ${A}/libs/ardour/session_butler.cc
-    ${A}/libs/ardour/session.cc
+    ${A}/send.cc
+    ${A}/session_bundles.cc
+    ${A}/session_butler.cc
+    ${A}/session.cc
+
+    ${L}/lua/lua.cc
 
     ${CMAKE_CURRENT_BINARY_DIR}/pbd/signals_generated.h
 )
@@ -105,18 +108,17 @@ target_include_directories(ardourlib PUBLIC
     ${LILV_INCLUDE_DIR}
     /usr/include/sigc++-2.0/
     /usr/lib/x86_64-linux-gnu/sigc++-2.0/include/
-    /usr/include/lilv-0/
-    ${A}/libs/pbd
-    ${A}/libs/temporal
-    ${A}/libs/ptformat
-    ${A}/libs/ardour
-    ${A}/libs/evoral
-    ${A}/libs/midi++2
-    ${A}/libs/zita-resampler
-    ${A}/libs/ctrl-interface/control_protocol
-    ${A}/libs/lua
-    ${A}/libs/audiographer
-    ${A}/libs/libltc/ltc/
+    ${L}/pbd
+    ${L}/temporal
+    ${L}/ptformat
+    ${L}/ardour
+    ${L}/evoral
+    ${L}/midi++2
+    ${L}/zita-resampler
+    ${L}/ctrl-interface/control_protocol
+    ${L}/lua
+    ${L}/audiographer
+    ${L}/libltc/ltc/
 )
 
 target_link_libraries(ardourlib PUBLIC
